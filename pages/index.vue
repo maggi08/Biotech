cdc0c7d771a021631b364605223999bbe767aec0
Magzhan Mussaiyn <maggi08mus@gmail.com>
2022-05-10 15:05:11 +0600

    Done

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial />
+  <div class="font-lake">
+    <BHeader />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
-}
+  name: "IndexPage",
+};
 </script>
+
+<style lang="post"></style>
